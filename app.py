4d689fd218fcaf2e95e10099ba2260e273cdb503
nlenno1 <nick.lennon@hotmail.co.uk>
2021-10-19 21:29:23 +0000

    connect flask app to mongodb and create test page to check its working

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
 import os
-from flask import Flask
+from flask import (
+    Flask, flash, render_template,
+    redirect, request, session, url_for)
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId  # to render the object id in a bson format to use in mongo db
 if os.path.exists("env.py"):
     import env
 
@@ -7,10 +11,20 @@ if os.path.exists("env.py"):
 # create an instance of Flask called app
 app = Flask(__name__)
 
+# retieving the hidden env variable for use in the app
+app.config["MONGO_DB"] = os.environ.get("MONGO_DB")
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
+
+# create instance of pymongo using contructor with app
+mongo = PyMongo(app)
+
 
 @app.route("/")
-def test():
-    return "Hello World. Testing that Heroku is connected up properly"
+@app.route("/get_all_genre")
+def get_all_genre():
+    genre_list = mongo.db.genre.find()
+    return render_template("genre-management.html", genre_list=genre_list)
 
 
 # application running instructions by retieving hidden env variables
