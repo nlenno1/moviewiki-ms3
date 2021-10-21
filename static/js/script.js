04da34035d4e6648fbbb7742b8c250831e3196f6
nlenno1 <nick.lennon@hotmail.co.uk>
2021-10-21 11:18:32 +0000

    feat: finish styling and animating sidenav bar

---
+++ static/js/script.js
@@ -0,0 +1,25 @@
+const sidenavToggler = document.querySelector("#sidenav-toggler")
+const sidenav = document.querySelector("#sidenav")
+const closeSidenav = document.querySelector("#sidenavClose")
+const contentCover = document.querySelector("#content-cover")
+
+function openNav() {
+    if (document.documentElement.clientWidth > 320) {
+        sidenav.style.width = "280px";
+    } else {
+        sidenav.style.width = "100%";
+    }
+    sidenav.style.borderLeft = "5px solid #EEBC1D";
+    
+    contentCover.style.width = "100%";
+    contentCover.style.opacity = "1";
+}
+
+function closeNav() {
+    sidenav.style.width = "0";
+    sidenav.style.borderLeft = "0px solid #EEBC1D";
+    contentCover.style.width = "0";
+    contentCover.style.opacity = "0";
+}
+
+
