a3ba38282ca358f1704621ebeac408d446d77c26
nlenno1 <nick.lennon@hotmail.co.uk>
2021-11-10 22:04:30 +0000

    feat: add form validation for sign up

--- static/js/password-validation.js
+++ static/js/password-validation.js
@@ -1,9 +1,9 @@
 $('document').ready (function () {
     $('#password, #password-confirm').on('keyup', function () {
         if ($('#password').val() == $('#password-confirm').val()) {
-          $('#password-feedback').html('Passwords Match').css('color', 'green');
+          $('#password-feedback').html('Passwords Match').css('color', '#000');
         } else 
-          $('#password-feedback').html('Passwords Dont Match').css('color', 'red');
+          $('#password-feedback').html('Passwords Dont Match').css('color', 'rgb(200, 0, 0)');
     });
 
 
