020ad4adb95bf05692f34aa64b47733a73235fb0
nlenno1 <nick.lennon@hotmail.co.uk>
2021-11-11 15:17:30 +0000

    feat: add js required attribute functionality to series-question-control

--- static/js/series-questions-control.js
+++ static/js/series-questions-control.js
@@ -1,16 +1,49 @@
 $('document').ready (function () {
-    console.log("jsloaded")
     // input disabled control on #series-questions section
     $("#start-series").on("click", function() {
+        //remove previous movie
         $("#previous-movie-container").attr('hidden', "")
+        $("#next-movie-name").removeAttr('required')
+        $("#previous-movie-name").val("");
+        // add next movie and make it required
         $("#next-movie-container").removeAttr('hidden')
+        $("#next-movie-name").attr('required', "")
+
     })
     $("#end-series").on("click", function() {
+        // remove next movie
         $("#next-movie-container").attr('hidden', "")
+        $("#previous-movie-name").removeAttr('required')
+        $("#next-movie-name").val("");
+        // add previous movie and make required
         $("#previous-movie-container").removeAttr('hidden')
+        $("#previous-movie-name").attr('required', "")
+
     })
     $("#middle-series").on("click", function() {
+        // make both inputs visable and required
         $("#next-movie-container").removeAttr('hidden')
+        $("#next-movie-name").attr('required', "")
         $("#previous-movie-container").removeAttr('hidden')
+        $("#previous-movie-name").attr('required', "")
+    })
+    $("#series-switch").on("click", function() {
+        if($('#series-name').prop('required')) {
+            // reset series form
+            $("#series-name").removeAttr('required')
+            $("#previous-movie-name").removeAttr('required')
+            $("#next-movie-name").removeAttr('required')
+            $("#start-series").prop('checked', false);
+            $("#middle-series").prop('checked', false);
+            $("#end-series").prop('checked', false);
+            $("#series-name").val("");
+            $("#next-movie-name").val("");
+            $("#previous-movie-name").val("");
+            $("#next-movie-container").attr('hidden', "")
+            $("#previous-movie-container").attr('hidden', "")
+        } else {
+            $("#series-name").attr('required', "")
+            console.log("Required Added")
+        }
     })
 })
